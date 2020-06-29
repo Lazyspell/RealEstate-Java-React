e12be30b82f9499f6dbe7605cb5ac148af06b397
Jeremy2975 <Jeremy2975@yahoo.com>
2020-06-29 09:27:07 -0700

    base project

---
+++ realestate-java-react/src/main/java/com/example/realestatejavareact/RealestateJavaReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.realestatejavareact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateJavaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestateJavaReactApplication.class, args);
+	}
+
+}
---
+++ realestate-java-react/src/test/java/com/example/realestatejavareact/RealestateJavaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realestatejavareact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateJavaReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
