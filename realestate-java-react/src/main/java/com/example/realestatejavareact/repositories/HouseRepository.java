581ef9d2c0d79a4d9d089b2047edb2ceed612768
Wagenheim <wagenheimk@me.com>
2020-07-15 13:31:54 -0400

    added a getById stub

--- realestate-java-react/src/main/java/com/example/realestatejavareact/repositories/HouseRepository.java
+++ realestate-java-react/src/main/java/com/example/realestatejavareact/repositories/HouseRepository.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface HouseRepository extends CrudRepository<House, Integer> {
 
+    House findById(int id);
+
     @Query("from House h where h.street_name = :streetName and h.street_number = :streetNumber")
     House findByStreetNameAndStreetNumber(String streetName, String streetNumber);
 
