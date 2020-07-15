43b6df96926cf98b017320b0c81ffe576bd11a11
Wagenheim <wagenheimk@me.com>
2020-07-15 11:50:33 -0400

    implemented get by city rank

--- realestate-java-react/src/main/java/com/example/realestatejavareact/services/CityService.java
+++ realestate-java-react/src/main/java/com/example/realestatejavareact/services/CityService.java
@@ -3,6 +3,7 @@ package com.example.realestatejavareact.services;
 
 import com.example.realestatejavareact.entities.City;
 import com.example.realestatejavareact.exceptions.BadRequestException;
+import com.example.realestatejavareact.exceptions.ResourceNotFoundException;
 import com.example.realestatejavareact.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class CityService {
 
         return ((List<City>) cityRepository.findAll());
     }
-//
+
 //    public City findById(int id){
 //
 //        if(id <= 0){
@@ -29,5 +30,19 @@ public class CityService {
 //        return City cityRepository.findById(id);
 //
 //    }
+
+    public City getByCityRank(int rank){
+        if(rank <= 0){
+            throw new BadRequestException("Invalid rank provided");
+        }
+
+        City retrievedCity = cityRepository.findByCityRank(rank);
+
+        if(retrievedCity == null){
+            throw new ResourceNotFoundException("No city found with that rank");
+        }
+
+        return retrievedCity;
+    }
 
 }
--- realestate-java-react/src/main/java/com/example/realestatejavareact/web/controllers/CityController.java
+++ realestate-java-react/src/main/java/com/example/realestatejavareact/web/controllers/CityController.java
@@ -4,10 +4,7 @@ package com.example.realestatejavareact.web.controllers;
 import com.example.realestatejavareact.entities.City;
 import com.example.realestatejavareact.services.CityService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -20,7 +17,6 @@ public class CityController {
 
     private CityService cityService;
 
-
     public CityController(CityService cityService){
         this.cityService = cityService;
     }
@@ -29,4 +25,7 @@ public class CityController {
     public List<City> getAllCities(HttpServletRequest req){
         return cityService.findAll();
     }
+
+    @GetMapping(value = "/rank/{rank}")
+    public City getByCityRank(@PathVariable int rank){return cityService.getByCityRank(rank);}
 }
