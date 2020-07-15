bb993a0db14a1d99f7d7bf848f2fdf9d4b8962f2
Wagenheim <wagenheimk@me.com>
2020-07-15 13:41:24 -0400

    added a getbyid endpoint

--- realestate-java-react/src/main/java/com/example/realestatejavareact/web/controllers/CityController.java
+++ realestate-java-react/src/main/java/com/example/realestatejavareact/web/controllers/CityController.java
@@ -3,6 +3,7 @@ package com.example.realestatejavareact.web.controllers;
 
 import com.example.realestatejavareact.entities.City;
 import com.example.realestatejavareact.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,7 @@ import java.util.List;
 @RequestMapping("/city")
 public class CityController {
 
+    @Autowired
     private CityService cityService;
 
     public CityController(CityService cityService){
@@ -25,6 +27,9 @@ public class CityController {
     public List<City> getAllCities(HttpServletRequest req){
         return cityService.findAll();
     }
+
+    @GetMapping(value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public City getById(@PathVariable int id){return cityService.findById(id);}
 
     @GetMapping(value = "/rank/{rank}", produces = MediaType.APPLICATION_JSON_VALUE)
     public City getByCityRank(@PathVariable int rank){return cityService.getByCityRank(rank);}
